965ca39eb8f65a277f9a637ca2074063f580684a
Rob Wilson <rob.wilson195@gmail.com>
2018-10-24 15:54:36 +0100

    Works, but unplayed yet.

--- game.rb
+++ game.rb
@@ -8,6 +8,33 @@ class Game
     @guessed_letters = []
   end
 
+  def add_to_guessed_letters(guessed_letter)
+    @guessed_letters << guessed_letter
+  end
 
+  def game_status
+    if @hidden_word.check_word_complete(@guessed_letters)
+      :won
+    elsif @player.lives <= 0
+      :loss
+    else
+      :continue
+    end
+  end
+
+  def game_loop
+    while game_status == :continue do
+      p "Make a guess!"
+      guessed_letter = puts.chomp
+      add_to_guessed_letters(guessed_letter)
+      @hidden_word.display_obscured_string
+      @player.lose_life unless @hidden_word.check_guess
+    end
+    if game_status == :win
+      p "Congratulations, you've won!"
+    else
+      p "Your snowman melted! Game OveR Loser!!!"
+    end
+  end
 
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -16,7 +16,7 @@ class TestGame < MiniTest::Test
     assert_equal(@player1, @game1.player)
   end
 
-  def test_game_has_hidden_word
+  def test_game_has_input_word
     assert_equal(@input_word1, @game1.hidden_word)
   end
 
@@ -24,6 +24,43 @@ class TestGame < MiniTest::Test
     assert_equal([], @game1.guessed_letters)
   end
 
+  def test_add_to_guessed_letters
+    @game1.add_to_guessed_letters("e")
+    assert_equal(["e"], @game1.guessed_letters)
+  end
+
+  def test_add_to_guessed_letters__multiple_present
+    @game1.add_to_guessed_letters("e")
+    @game1.add_to_guessed_letters("f")
+    assert_equal(["e","f"], @game1.guessed_letters)
+  end
+
+  def test_game_status__win
+    @game1.add_to_guessed_letters("p")
+    @game1.add_to_guessed_letters("o")
+    @game1.add_to_guessed_letters("t")
+    @game1.add_to_guessed_letters("a")
+
+    assert_equal(:won, @game1.game_status)
+  end
+
+  def test_game_status__loss
+    6.times do
+      @player1.lose_life
+    end
+    assert_equal(:loss, @game1.game_status)
+  end
+
+  def test_game_status__continue
+    3.times do
+      @player1.lose_life
+    end
+    assert_equal(:continue, @game1.game_status)
+  end
+
+  def play
+    @game1.game_loop
+  end
 
 
 end
